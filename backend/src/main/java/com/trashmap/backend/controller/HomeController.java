e76dc240788c97d8c3152312b7923182a5781db9
KimSiTek <sihuk38@gachon.ac.kr>
2025-05-05 19:50:20 +0900

    Create HomeController.java

---
+++ backend/src/main/java/com/trashmap/backend/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.trashmap.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 Spring Boot 서버 정상 작동 중!";
+    }
+}
